846ccc1417007211d83b39c9b3616e82a3fadc76
Jignesh <jignesh.sharma.707@gmail.com>
2023-05-15 12:45:28 +0530

    react-portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import './assets/js/script';
+import About from './component/About/About';
+import Contact from './component/Contact/Contact';
+import Footer from './component/Footer/Footer';
+import Header from './component/Header/Header';
+import Newsletter from './component/NewsLetter/NewsLetter';
+import Services from './component/Services/Services';
+import Testimonials from './component/Testimonials/Testimonials';
+import Work from './component/Work/Work';
+import WorkProcess from './component/WorkProcess/WorkProcess';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Services />
+      <Work />
+      <WorkProcess />
+      <Newsletter />
+      <About />
+      {/* <Testimonials /> */}
+      <Contact />
+      <Footer />
     </div>
   );
 }
